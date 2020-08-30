502d61eaa7ef160034895f64cf61915d34c3684f
hazelrasheed@gmail.com <hazelrasheed@gmail.com>
2020-08-30 21:05:18 +0300

    WORKING..

--- src/Articles_app/models.py
+++ src/Articles_app/models.py
@@ -63,6 +63,15 @@ class Articles_model(models.Model):
             lst.append(i[1])
         return lst
 
+    def save_model(self, request, instance, form, change):
+        user = request.user
+        instance = form.save(commit=False)
+        if not change or not instance.created_by:
+            instance.created_by = user
+        instance.modified_by = user
+        instance.save()
+        form.save_m2m()
+        return instance
 
 
 class Category(models.Model):
